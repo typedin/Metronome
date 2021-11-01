350a59b7d366b91ce2a34202e4f105a5019a390c
Antoine Cheynet <antoine@typedin.dev>
2021-11-01 18:35:18 +0100

    App: wip
    garbage out
    
    garbage out

--- App.tsx
+++ App.tsx
@@ -1,50 +1,73 @@
-import Metronome from "./src/MetronomeClass";
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { Pressable, View, Text } from "react-native";
+import { Pressable, Text } from "react-native";
 import Container from "./src/layouts/Container";
 import tw from "tailwind-react-native-classnames";
-import BpmToMS from "./src/services/BpmToMS";
-
-type State = {
-  intervalID: any | null;
-  isPlaying: boolean;
-  loopInterval: number;
-  tempo: Bpm;
-};
+import { webPlayer } from "./src/services/players/MetronomeSoundPlayer";
+import createMetronome from "./src/Metronome";
 
 const DEFAULT_TEMPO = 60;
 export default function App() {
-  const metronome = new Metronome(DEFAULT_TEMPO);
-
-  const [state, setState] = useState<State>({
-    intervalID: null,
-    isPlaying: false,
-    loopInterval: 1000,
-    tempo: DEFAULT_TEMPO,
-  });
-
-  const changeTempo = ({ action }: { action: string }) => {
-    const tempo = action === "increment" ? state.tempo + 1 : state.tempo - 1;
-
-    setState({
-      ...state,
-      tempo,
-      loopInterval: BpmToMS({
-        bpm: tempo,
-      }).toMS(),
-    });
-  };
+  const [metronome] = useState(createMetronome(DEFAULT_TEMPO, webPlayer));
+  const [isRunning, setIsRunning] = useState(false);
 
   return (
     <Container>
       <Pressable
-        onPress={() =>
-          metronome.isRunning ? metronome.stop() : metronome.start()
+        onPress={
+          isRunning
+            ? function () {
+                metronome.stop();
+                setIsRunning(metronome.isRunning);
+              }
+            : function () {
+                metronome.start();
+                setIsRunning(metronome.isRunning);
+              }
         }
-        style={tw`mt-16`}
       >
-        <Text>Here</Text>
+        {metronome.isRunning && (
+          <svg
+            style={tw`w-9 h-9`}
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+        )}
+        {!metronome.isRunning && (
+          <svg
+            style={tw`w-9 h-9`}
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+            ></path>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+        )}
+        <Text style={tw`flex flex-row justify-center items-center`}>
+          <span style={tw`font-bold text-gray-800`}>
+            {metronome.isRunning ? "Stop" : "Start"}
+          </span>
+        </Text>
       </Pressable>
     </Container>
   );
